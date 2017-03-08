75566a63ea533cf7167f040f3f2b351f0880a89e
PaweÅ Kubiak <pawel.kubiak@student.uj.edu.pl>
2017-03-08 12:39:59 +0100

    Task 2: Cameras - final

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,37 +1,26 @@
-#include "../core/image.h"
-#include "../rt/cameras/perspective.h"
-#include "../rt/cameras/orthographic.h"
-#include "../rt/renderer.h"
+#include <rt/cameras/perspective.h>
+#include <rt/cameras/orthographic.h>
+#include <core/image.h>
+#include <core/vector.h>
+#include <core/point.h>
+#include <rt/renderer.h>
 
 using namespace rt;
 
-int main(int argc, char* argv[])
-{
-  const float deg = 3.1415926/180.0;
-  Image img(300, 200);
-  PerspectiveCamera cam_persp(
-    Point(0,0,0),
-    Vector(0,0,-0.1),
-    Vector(0,1,0),
-    60*deg,
-    60*deg
-  );
+int main() {
+    Image img(300, 200);
+    {
+    PerspectiveCamera cam(Point(0, 0, 10), Vector(0, 0, -1), Vector(0, 1, 0), pi/4, pi/3);
+    Renderer engine(&cam, nullptr);
+    engine.render(img);
+    img.writePNG("a2-1.png");
+    }
 
-  Renderer engine(&cam_persp, NULL);
-  engine.render(img);
-  img.writePNG("a2-perspective.png");
-
-  OrthographicCamera cam_orth(
-    Point(0,0,0),
-    Vector(0,0,-0.1),
-    Vector(0,1,0),
-    1.0,
-    1.0
-  );
-
-  Renderer engine2(&cam_orth, NULL);
-  engine2.render(img);
-  img.writePNG("a2-orthographic.png");
-
-  return 0;
+    {
+    OrthographicCamera cam(Point(0, 0, 10), Vector(0, 0, -1), Vector(0, 1, 0), 10.0f, 10.0f);
+    Renderer engine(&cam, nullptr);
+    engine.render(img);
+    img.writePNG("a2-2.png");
+    }
+    return 0;
 }
