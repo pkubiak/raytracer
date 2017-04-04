24d9d400cceaaa423fe5f64ace8980f2512f11b2
Paweł Kubiak <pawel.kubiak@student.uj.edu.pl>
2017-04-04 09:01:16 +0200

    Upload src for 'Materials'

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -5,17 +5,22 @@
 #include <rt/world.h>
 #include <rt/renderer.h>
 
-
 #include <rt/cameras/perspective.h>
 #include <rt/solids/quad.h>
+#include <rt/solids/sphere.h>
 #include <rt/groups/simplegroup.h>
 #include <rt/materials/dummy.h>
 
 #include <rt/lights/pointlight.h>
-#include <rt/lights/spotlight.h>
-#include <rt/lights/directional.h>
 
-#include <rt/integrators/raytrace.h>
+#include <rt/textures/constant.h>
+
+#include <rt/materials/lambertian.h>
+#include <rt/materials/phong.h>
+#include <rt/materials/mirror.h>
+#include <rt/materials/combine.h>
+
+#include <rt/integrators/recraytrace.h>
 
 using namespace rt;
 
@@ -30,49 +35,114 @@ void makeBox(Group* scene, const Point& aaa, const Vector& forward, const Vector
     scene->add(new Quad(bbb, -left, -up, texMapper, material));
 }
 
-void renderCornellboxB(float scale, const char* filename) {
-    Image img(2400, 2400);
+Texture* redtex;
+Texture* greentex;
+Texture* bluetex;
+Texture* blacktex;
+Texture* whitetex;
+
+void initTextures() {
+    redtex = new ConstantTexture(RGBColor(.7f,0.f,0.f));
+    greentex = new ConstantTexture(RGBColor(0.f,.7f,0.f));
+    bluetex = new ConstantTexture(RGBColor(0.f,0.f,0.7f));
+    blacktex = new ConstantTexture(RGBColor::rep(0.0f));
+    whitetex = new ConstantTexture(RGBColor::rep(1.0f));
+}
+
+}
+
+void prepMaterials1(Material** materials) {
+    materials[0] = new LambertianMaterial(blacktex, whitetex);
+    materials[1] = new LambertianMaterial(blacktex, redtex);
+    materials[2] = new LambertianMaterial(blacktex, greentex);
+
+    materials[3] = new LambertianMaterial(blacktex, whitetex);
+
+    materials[4] = new LambertianMaterial(blacktex, whitetex);
+}
+
+void prepMaterials2(Material** materials) {
+    materials[0] = new LambertianMaterial(blacktex, whitetex);
+    materials[1] = new LambertianMaterial(blacktex, redtex);
+    materials[2] = new LambertianMaterial(blacktex, greentex);
+
+    materials[3] = new PhongMaterial(whitetex, 10.0f);
+    materials[4] = new MirrorMaterial(0.0f, 0.0f);
+}
+
+
+void prepMaterials3(Material** materials) {
+    materials[0] = new LambertianMaterial(blacktex, whitetex);
+    materials[1] = new LambertianMaterial(blacktex, redtex);
+
+    CombineMaterial* green = new CombineMaterial();
+    green->add(new LambertianMaterial(blacktex, greentex), 0.5f);
+    green->add(new PhongMaterial(whitetex, 2.0f), 0.5f);
+    materials[2] = green;
+
+    materials[3] = new MirrorMaterial(2.485, 3.433);
+
+    MirrorMaterial* mirror = new MirrorMaterial(0.0f, 0.0f);
+    PhongMaterial* phong = new PhongMaterial(whitetex, 10.0f);
+    CombineMaterial* combined = new CombineMaterial();
+    combined->add(materials[0],0.2f);
+    combined->add(phong,0.62f);
+    combined->add(mirror,0.18f);
+    materials[4] = combined;
+}
+
+void renderCornellbox(float scale, const char* filename, Material** materials) {
+    Image img(400, 400);
     World world;
     SimpleGroup* scene = new SimpleGroup();
     world.scene = scene;
 
     PerspectiveCamera cam(Point(278*scale, 273*scale, -800*scale), Vector(0, 0, 1), Vector(0, 1, 0), 0.686f, 0.686f);
 
-    DummyMaterial* mat = new DummyMaterial();
 
-    scene->add(new Quad(Point(000.f,000.f,000.f)*scale, Vector(550.f,000.f,000.f)*scale, Vector(000.f,000.f,560.f)*scale, nullptr, mat)); //floor
-    scene->add(new Quad(Point(000.f,550.f,000.f)*scale, Vector(550.f,000.f,000.f)*scale, Vector(000.f,000.f,560.f)*scale, nullptr, mat)); //ceiling
-    scene->add(new Quad(Point(000.f,000.f,560.f)*scale, Vector(550.f,000.f,000.f)*scale, Vector(000.f,550.f,000.f)*scale, nullptr, mat)); //back wall
-    scene->add(new Quad(Point(000.f,000.f,000.f)*scale, Vector(000.f,000.f,560.f)*scale, Vector(000.f,550.f,000.f)*scale, nullptr, mat)); //right wall
-    scene->add(new Quad(Point(550.f,000.f,000.f)*scale, Vector(000.f,000.f,560.f)*scale, Vector(000.f,550.f,000.f)*scale, nullptr, mat)); //left wall
+    Material* grey = materials[0];
+    Material* leftWallMaterial = materials[1];
+    Material* rightWallMaterial = materials[2];
+    
+    Material* sphereMaterial = materials[3];
+    Material* floorMaterial = materials[4];
 
-    //short box
-    makeBox(scene, Point(082.f, 000.1f, 225.f)*scale, Vector(158.f, 000.f, 047.f)*scale, Vector(048.f, 000.f, -160.f)*scale, Vector(000.f, 165.f, 000.f)*scale, nullptr, mat);
+    scene->add(new Quad(Point(000.f,000.f,000.f)*scale, Vector(550.f,000.f,000.f)*scale, Vector(000.f,000.f,560.f)*scale, nullptr, floorMaterial)); //floor
+    scene->add(new Quad(Point(550.f,550.f,000.f)*scale, Vector(-550.f,000.f,000.f)*scale, Vector(000.f,000.f,560.f)*scale, nullptr, grey)); //ceiling
+    scene->add(new Quad(Point(000.f,000.f,560.f)*scale, Vector(550.f,000.f,000.f)*scale, Vector(000.f,550.f,000.f)*scale, nullptr, grey)); //back wall
+    scene->add(new Quad(Point(000.f,000.f,000.f)*scale, Vector(000.f,000.f,560.f)*scale, Vector(000.f,550.f,000.f)*scale, nullptr, rightWallMaterial)); //right wall
+    scene->add(new Quad(Point(550.f,550.f,000.f)*scale, Vector(000.f,000.f,560.f)*scale, Vector(000.f,-550.f,000.f)*scale, nullptr, leftWallMaterial)); //left wall
+
+    scene->add(new Sphere(Point(150.0f, 100.0f, 150.0f)*scale, 99.0f*scale, nullptr, sphereMaterial));
 
     //tall box
-    makeBox(scene, Point(265.f, 000.1f, 296.f)*scale, Vector(158.f, 000.f, -049.f)*scale, Vector(049.f, 000.f, 160.f)*scale, Vector(000.f, 330.f, 000.f)*scale, nullptr, mat);
+    makeBox(scene, Point(265.f, 000.1f, 296.f)*scale, Vector(158.f, 000.f, -049.f)*scale, Vector(049.f, 000.f, 160.f)*scale, Vector(000.f, 330.f, 000.f)*scale, nullptr, grey);
 
     //point light
-    world.light.push_back(new PointLight(Point(288*scale,529.99f*scale,279.5f*scale),RGBColor::rep(20000.0f*scale*scale)));
+    world.light.push_back(new PointLight(Point((278)*scale,529.99f*scale,(279.5f)*scale),RGBColor::rep(150000.0f*scale*scale)));
+    world.light.push_back(new PointLight(Point((278)*scale,229.99f*scale,(-359.5f)*scale),RGBColor::rep(50000.0f*scale*scale)));
 
-    //spot light
-    world.light.push_back(new SpotLight(Point(70.f, 400.f, 230.f)*scale, Vector(0.0f, -1.0f, 0.0f),  pi/4, 8.0f, RGBColor(0,60000.0f*scale*scale,0)));
-    world.light.push_back(new SpotLight(Point(520.f, 300.f, 230.f)*scale, Vector(-1.0f, -1.0f, 0.0f),  pi/3, 3.0f, RGBColor(60000.0f*scale*scale,0,0)));
+    world.light.push_back(new PointLight(Point(490*scale,159.99f*scale,279.5f*scale),RGBColor(40000.0f*scale*scale,0,0)));
+    world.light.push_back(new PointLight(Point(40*scale,159.99f*scale,249.5f*scale),RGBColor(5000.0f*scale*scale,30000.0f*scale*scale,5000.0f*scale*scale)));
 
-    //directional light
-    DirectionalLight dirl(Vector(0.2f,-0.5f,0.5f).normalize(), RGBColor(0.25f,0.25f,0.5f));
-    world.light.push_back(&dirl);
-
-    RayTracingIntegrator integrator(&world);
+    RecursiveRayTracingIntegrator integrator(&world);
 
     Renderer engine(&cam, &integrator);
     engine.render(img);
     img.writePNG(filename);
 }
-}
 
-int main(int argc, char* argv[]) {
-    renderCornellboxB(0.001f, "a4-3.png");
-    renderCornellboxB(0.01f, "a4-4.png");
+
+int main() {
+    Material** materials = new Material*[5];
+    initTextures();
+    prepMaterials1(materials);
+    renderCornellbox(0.001f, "a5-1.png", materials);
+    prepMaterials2(materials);
+    renderCornellbox(0.001f, "a5-2.png", materials);
+    prepMaterials3(materials);
+    renderCornellbox(0.001f, "a5-3.png", materials);
+    delete [] materials;
     return 0;
 }
+
---
+++ src/rt/integrators/recraytrace.h
@@ -0,0 +1,23 @@
+#ifndef CG1RAYTRACER_INTEGRATORS_RECURSIVERAYTRACING_HEADER
+#define CG1RAYTRACER_INTEGRATORS_RECURSIVERAYTRACING_HEADER
+
+#include <rt/integrators/integrator.h>
+
+namespace rt {
+
+class World;
+class Ray;
+class RGBColor;
+
+class RecursiveRayTracingIntegrator : public Integrator {
+public:
+    RecursiveRayTracingIntegrator(World* world) : Integrator(world) {}
+    virtual RGBColor getRadiance(const Ray& ray) const;
+private:
+    RGBColor getRadiance(const Ray& ray, int currentRecursionDepth) const;
+    static const int maxRecursionDepth = 6;
+};
+
+}
+
+#endif
