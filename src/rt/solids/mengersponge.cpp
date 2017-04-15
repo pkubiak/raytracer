7ef313b5fe80699e22325e6443087dfa55b3b8d0
Paweł Kubiak <pawel.kubiak@student.uj.edu.pl>
2017-04-15 23:57:36 +0200

    Speedup mengersponge rendering

--- src/rt/solids/mengersponge.cpp
+++ src/rt/solids/mengersponge.cpp
@@ -15,26 +15,34 @@ namespace rt {
   pair<float, Vector> MengerSponge::intersect(const Ray& ray, float previousBestDistance, Point p, Vector v1, Vector v2, Vector v3, int depth) {
     // printf("%d\n", depth);
     if(depth == 0)
-      // return make_pair(INFINITY,Vector());
       return Parallelepiped::intersect(ray, previousBestDistance, p, v1,v2,v3);
 
-    pair<float,Vector> best = make_pair(INFINITY, Vector()), ints;
+    vector<tuple<float, int,int,int> > res;
 
     for(int x=0;x<3;x++)
       for(int y=0;y<3;y++)
         for(int z=0;z<3;z++){
-          if(x*y==1||y*z==1||z*x==1)
+          // if(x*y==1||y*z==1||z*x==1)
+          if(!((x==1||y==1||z==1)&&(x*y*z!=1)))
             continue;
-          ints = Parallelepiped::intersect(ray, previousBestDistance, p+(0.3333333*x)*v1 + (0.3333333*y)*v2 + (0.3333333*z)*v3, (1.0/3.0)*v1, (1.0/3.0)*v2, (1.0/3.0)*v3);
+          auto ints = Parallelepiped::intersect(ray, previousBestDistance, p+(0.3333333*x)*v1 + (0.3333333*y)*v2 + (0.3333333*z)*v3, (1.0/3.0)*v1, (1.0/3.0)*v2, (1.0/3.0)*v3);
           if(ints.first==INFINITY)
             continue;
-          ints = MengerSponge::intersect(ray, previousBestDistance, p+(0.3333333*x)*v1 + (0.3333333*y)*v2 + (0.3333333*z)*v3, (1.0/3.0)*v1, (1.0/3.0)*v2, (1.0/3.0)*v3, depth-1);
-          if(ints.first < previousBestDistance){
-            previousBestDistance = ints.first;
-            best = ints;
-          }
+          res.push_back(make_tuple(ints.first, x, y, z));
         }
-    return best;
+
+    sort(res.begin(), res.end());
+
+    float t;int x,y,z;
+
+    for(auto pr: res){
+      std::tie(t, x, y, z) = pr;
+      auto ints = MengerSponge::intersect(ray, previousBestDistance, p+(0.3333333*x)*v1 + (0.3333333*y)*v2 + (0.3333333*z)*v3, (1.0/3.0)*v1, (1.0/3.0)*v2, (1.0/3.0)*v3, depth-1);
+      if(ints.first != INFINITY)
+        return ints;
+    }
+
+    return make_pair(INFINITY, Vector());
   }
 
   Intersection MengerSponge::intersect(const Ray& ray, float previousBestDistance) const {
