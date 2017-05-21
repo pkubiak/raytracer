8f1c09b8dabd0f7cb7d263e8d82feb34f83da03b
Paweł Kubiak <pawel.kubiak@student.uj.edu.pl>
2017-05-21 12:34:24 +0200

    Upload src for 'Instancing'.

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,113 +1,114 @@
 
-#include <core/assert.h>
-#include <core/scalar.h>
 #include <core/image.h>
+#include <rt/cameras/perspective.h>
+#include <rt/groups/group.h>
+#include <rt/groups/simplegroup.h>
+#include <rt/primmod/instance.h>
+#include <rt/integrators/casting.h>
 #include <rt/world.h>
 #include <rt/renderer.h>
 
-#include <cmath>
+#include <rt/solids/infiniteplane.h>
+#include <rt/solids/quad.h>
+#include <rt/solids/sphere.h>
 
-#include <rt/groups/simplegroup.h>
-#include <rt/cameras/perspective.h>
-#include <rt/primmod/bmap.h>
-#include <rt/textures/imagetex.h>
-#include <rt/solids/triangle.h>
-#include <rt/integrators/casting.h>
 
 using namespace rt;
 
-void bump(Camera* cam, Texture* bumptex, const char* filename) {
-    static const float scale = 0.001f;
-    static const float bumbscale = 0.008f;
-    Image img(800, 800);
-    World world;
+void addTree(Group* g, int trunkTessel, float trunkHeight, float trunkRadius, float crownHeight, float crownRadius, int crownTessel, int crownSteps) {
+
+    Vector ex = Vector(1.0f, 0.0f, 0.0f);
+    Vector ey = Vector(0.0f, 0.0f, 1.0f);
+    Vector ez = Vector(0.0f, 1.0f, 0.0f);
+    //trunk
+    float trunkStep= (2*pi) / trunkTessel;
+
+    for (int i=0; i<trunkTessel; ++i) {
+        float angle = i * trunkStep;
+        float angleNext = (i+1) * trunkStep;
+        
+        Point start( trunkRadius * sin(angle), 0.0f, trunkRadius * cos(angle));
+        Point end( trunkRadius * sin(angleNext), 0.0f, trunkRadius * cos(angleNext));
+        g->add(new Quad(start, end-start, Vector(0.0f, trunkHeight, 0.0f), nullptr, nullptr));
+    }
+
+    float crownStep = (2*pi) / crownTessel;
+
+    //crown
+    for (int i=0; i<crownSteps; ++i) {
+        float height = trunkHeight + i * crownHeight / crownSteps;
+        
+        float x = float(crownSteps-i-1)/crownSteps;
+        //float stepRadius = crownRadius * sin(2.5f * sqr(x));
+        float stepRadius = crownRadius * sin(x*pi);
+        if (stepRadius>0.0f) {
+        float sphereRadius = 1.5f*pi * stepRadius / crownTessel;
+        for (int j=0; j<crownTessel; ++j) {
+            float angle = j * crownStep;
+            g->add(new Sphere(
+                Point(stepRadius * sin(angle), height, stepRadius * cos(angle)),
+                sphereRadius,
+                nullptr, nullptr));
+        }
+        }
+        g->add(new Sphere(Point(0.0f, trunkHeight, 0.0f), trunkRadius, nullptr, nullptr));
+        g->add(new Sphere(Point(0.0f, trunkHeight + crownHeight - crownStep*1.5, 0.0f), trunkRadius, nullptr, nullptr));
+    }
+
+}
+
+int main() {
+    Image img(800, 600);
+
+    SimpleGroup* tree = new SimpleGroup();
+    addTree(tree, 16, 3.0f, 0.5f, 5.0f, 2.0f, 8, 8);
+    tree->rebuildIndex();
+
     SimpleGroup* scene = new SimpleGroup();
+
+    Instance* normal = new Instance(tree);
+    scene->add(normal);
+
+    float circleRadius = 18.0f;
+    for (int i = 0; i<11; ++i) {
+        float angle = 2.0f*i*pi / 11.0f;
+        Instance* itree = new Instance(tree);
+        itree->scale(Vector(1.0f,1.0f + sin(i*1.0f)*0.3f,1.0f));
+        itree->translate(Vector(sin(angle) * circleRadius, 0.0f, cos(angle) * circleRadius));
+        scene->add(itree);
+    }
+
+    {
+    Instance* broken = new Instance(tree);
+    broken->rotate(Vector(0.3f, 0.0f, 0.7f),-1.0f);
+    broken->translate(Vector(5.0f, -0.5f, 0.0f));
+    scene->add(broken);
+    }
+
+    {
+    Instance* broken = new Instance(tree);
+    broken->rotate(Vector(0.7f, 0.0f, -0.3f),-1.0f);
+    broken->translate(Vector(5.0f, -0.5f, 0.0f));
+    scene->add(broken);
+    }
+
+    {
+    Instance* flat = new Instance(tree);
+    flat->scale(Vector(0.3f, 1.5f, 1.5f));
+    flat->translate(Vector(-11.0f, 0.0f, 0.0f));
+    scene->add(flat);
+    }
+
+    scene->add(new InfinitePlane(Point(0,0,0), Vector(0, 1, 0), nullptr, nullptr));
+    scene->rebuildIndex();
+
+    World world;
     world.scene = scene;
-
-
-
-    //floor
-    scene->add(
-        new BumpMapper(
-            new Triangle(Point(000.f,000.f,000.f)*scale, Point(000.f,000.f,560.f)*scale, Point(550.f,000.f,000.f)*scale, nullptr, nullptr),
-            bumptex, Point(0.0f,0.0f,0.0f), Point(0.0f, 1.0f, 0.0f), Point(1.0f, 0.0f, 0.0f), bumbscale)
-            );
-    scene->add(
-        new BumpMapper(
-            new Triangle(Point(550.f,000.f,560.f)*scale, Point(550.f,000.f,000.f)*scale, Point(000.f,000.f,560.f)*scale, nullptr, nullptr),
-            bumptex, Point(1.0f,1.0f,0.0f), Point(1.0f, 0.0f, 0.0f), Point(0.0f, 1.0f, 0.0f), bumbscale)
-            );
-
-    //ceiling
-    scene->add(
-        new BumpMapper(
-            new Triangle(Point(000.f,550.f,000.f)*scale, Point(550.f,550.f,000.f)*scale, Point(000.f,550.f,560.f)*scale, nullptr, nullptr),
-            bumptex, Point(0.0f,0.0f,0.0f), Point(1.0f, 0.0f, 0.0f), Point(0.0f, 1.0f, 0.0f), bumbscale)
-        );
-    scene->add(
-        new BumpMapper(
-            new Triangle(Point(550.f,550.f,560.f)*scale, Point(000.f,550.f,560.f)*scale, Point(550.f,550.f,000.f)*scale, nullptr, nullptr),
-            bumptex, Point(1.0f,1.0f,0.0f), Point(0.0f, 1.0f, 0.0f), Point(1.0f, 0.0f, 0.0f), bumbscale)
-        );
-
-    //back wall
-    scene->add(
-        new BumpMapper(
-            new Triangle(Point(000.f,000.f,560.f)*scale, Point(000.f,550.f,560.f)*scale, Point(550.f,000.f,560.f)*scale, nullptr, nullptr),
-            bumptex, Point(0.0f,0.0f,0.0f), Point(0.0f, 1.0f, 0.0f), Point(1.0f, 0.0f, 0.0f), bumbscale)
-        );
-    scene->add(
-        new BumpMapper(
-            new Triangle(Point(550.f,550.f,560.f)*scale, Point(550.f,000.f,560.f)*scale, Point(000.f,550.f,560.f)*scale, nullptr, nullptr),
-            bumptex, Point(1.0f,1.0f,0.0f), Point(1.0f, 0.0f, 0.0f), Point(0.0f, 1.0f, 0.0f), bumbscale)
-            );
-
-    //right wall
-    scene->add(
-        new BumpMapper(
-            new Triangle(Point(000.f,000.f,000.f)*scale, Point(000.f,550.f,000.f)*scale, Point(000.f,000.f,560.f)*scale, nullptr, nullptr),
-            bumptex, Point(0.0f,0.0f,0.0f), Point(0.0f, 1.0f, 0.0f), Point(1.0f, 0.0f, 0.0f), bumbscale)
-        );
-    scene->add(
-        new BumpMapper(
-            new Triangle(Point(000.f,550.f,560.f)*scale, Point(000.f,000.f,560.f)*scale, Point(000.f,550.f,000.f)*scale, nullptr, nullptr),
-            bumptex, Point(1.0f,1.0f,0.0f), Point(1.0f, 0.0f, 0.0f), Point(0.0f, 1.0f, 0.0f), bumbscale)
-            );
-
-    //left wall
-    scene->add(
-        new BumpMapper(
-            new Triangle(Point(550.f,000.f,000.f)*scale, Point(550.f,000.f,560.f)*scale, Point(550.f,550.f,000.f)*scale, nullptr, nullptr),
-            bumptex, Point(0.0f,0.0f,0.0f), Point(1.0f, 0.0f, 0.0f), Point(0.0f, 1.0f, 0.0f), bumbscale)
-            );
-    scene->add(
-        new BumpMapper(
-            new Triangle(Point(550.f,550.f,560.f)*scale, Point(550.f,550.f,000.f)*scale, Point(550.f,000.f,560.f)*scale, nullptr, nullptr),
-            bumptex, Point(1.0f,1.0f,0.0f), Point(0.0f, 1.0f, 0.0f), Point(1.0f, 0.0f, 0.0f), bumbscale)
-        );
-
     RayCastingIntegrator integrator(&world);
 
-    Renderer engine(cam, &integrator);
+    PerspectiveCamera cam(Point(-3.75f, 20, 40), Vector(0.1, -0.5, -1), Vector(0, 1, 0), pi/4, pi/3);
+    Renderer engine(&cam, &integrator);
     engine.render(img);
-    engine.setSamples(9);
-    img.writePNG(filename);
+    img.writePNG("a7-1.png");
 }
 
-
-
-// void main_gradient_texture();
-// void main_cooktorrance();
-int main() {
-    ImageTexture* bumptex = new ImageTexture("models/stones_bump.png", ImageTexture::REPEAT, ImageTexture::NEAREST);
-    ImageTexture* bumptexsmooth = new ImageTexture("models/stones_bump.png", ImageTexture::REPEAT, ImageTexture::BILINEAR);
-    Camera* cam1 = new PerspectiveCamera(Point(0.048f, 0.043f, -0.050f), Vector(-0.3f, -0.3f, 0.7f), Vector(0, 1, 0), 0.686f, 0.686f);
-    bump(cam1, bumptex, "bump-1.png");
-    bump(cam1, bumptexsmooth, "bump-2.png");
-    Camera* cam2 = new PerspectiveCamera(Point(0.278f, 0.273f, -0.800f), Vector(0, 0, 1), Vector(0, 1, 0), 0.686f, 0.686f);
-    bump(cam2, bumptexsmooth, "bump-3.png");
-    // main_gradient_texture();
-    // main_cooktorrance();
-
-    return 0;
-}
