b92a702674cff684459e5b9048fc2262cc7cd7fd
Paweł Kubiak <pawel.kubiak@student.uj.edu.pl>
2017-06-09 00:34:59 +0200

    Cleanup classes

--- src/rt/groups/simplegroup.h
+++ src/rt/groups/simplegroup.h
@@ -1,25 +1,20 @@
-#ifndef CG1RAYTRACER_GROUPS_SIMPLEGROUP_HEADER
-#define CG1RAYTRACER_GROUPS_SIMPLEGROUP_HEADER
+#pragma once
 
 #include <vector>
 #include <rt/groups/group.h>
 
 namespace rt {
+  class SimpleGroup : public Group {
+    private:
+      std::vector<Primitive*> primitives;
 
-class SimpleGroup : public Group {
-private:
-    std::vector<Primitive*> primitives;
-
-public:
-    SimpleGroup(): Group() {}
-    virtual BBox getBounds() const;
-    virtual Intersection intersect(const Ray& ray, float previousBestDistance=FLT_MAX) const;
-    virtual void rebuildIndex();
-    virtual void add(Primitive* p);
-    virtual void setMaterial(Material* m);
-    virtual void setCoordMapper(CoordMapper* cm);
-};
-
+    public:
+      SimpleGroup(): Group() {}
+      virtual BBox getBounds() const;
+      virtual Intersection intersect(const Ray& ray, float previousBestDistance=FLT_MAX) const;
+      virtual void rebuildIndex();
+      virtual void add(Primitive* p);
+      virtual void setMaterial(Material* m);
+      virtual void setCoordMapper(CoordMapper* cm);
+  };
 }
-
-#endif
--- src/rt/solids/disc.h
+++ src/rt/solids/disc.h
@@ -1,28 +1,22 @@
-#ifndef CG1RAYTRACER_SOLIDS_DISC_HEADER
-#define CG1RAYTRACER_SOLIDS_DISC_HEADER
+#pragma once
 
 #include <rt/solids/solid.h>
 #include <core/point.h>
 
 namespace rt {
+  class Disc : public Solid {
+    private:
+      Point center;
+      Vector normal;
+      float radius;
 
-class Disc : public Solid {
-private:
-  Point center;
-  Vector normal;
-  float radius;
+    public:
+      Disc() {}
+      Disc(const Point& center, const Vector& normal, float radius, CoordMapper* texMapper, Material* material);
 
-public:
-    Disc() {}
-    Disc(const Point& center, const Vector& normal, float radius, CoordMapper* texMapper, Material* material);
-
-    virtual BBox getBounds() const;
-    virtual Intersection intersect(const Ray& ray, float previousBestDistance=FLT_MAX) const;
-    virtual Point sample() const;
-    virtual float getArea() const;
-};
-
+      virtual BBox getBounds() const;
+      virtual Intersection intersect(const Ray& ray, float previousBestDistance=FLT_MAX) const;
+      virtual Point sample() const;
+      virtual float getArea() const;
+  };
 }
-
-
-#endif
